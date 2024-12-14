5c603eb34d4661ea4d80701b84d3ce5e62ef0b10
Techie <anuragsinghm08@gmail.com>
2024-12-14 23:43:04 +0530

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import { Children } from 'react'
+
+function Button({
+    children,
+    type="button",
+    textColor="text-white",
+    bgColor = "bg-blue-600",
+    className= "",
+    ...props
+}
+) {
+  return (
+    <button className={`px-4 py-2 rounded-s-lg ${bgColor} ${textColor} ${className}`} {...props}>
+        {children}
+    </button>
+  )
+}
+
+export default Button
---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div>
+      <img src="https://png.pngtree.com/png-vector/20230922/ourmid/pngtree-tattoo-tribal-logo-vector-illustration-to-generative-ai-png-image_10153024.png" alt="logo"
+      height="60px" width="70px" 
+      />
+    </div>
+  )
+}
+
+export default Logo
